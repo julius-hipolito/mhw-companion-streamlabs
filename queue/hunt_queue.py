10cee43f4880db0a0480b10f4ea0aed5220e3dd3
Julius Hipolito <jc.hipolito@gmail.com>
2019-04-15 23:35:16 -0700

    Change huntQueue to deque collection.
    Fix #20

--- queue/hunt_queue.py
+++ queue/hunt_queue.py
@@ -12,19 +12,22 @@ class HuntQueue:
 	def __init__(self, maxsize, weaponsRollCount, monstersRollCount):
 		weaponCount = max(min(weaponsRollCount-2, 100), 1)
 		monsterCount = max(min(monstersRollCount-2, 100), 1)
-
-		self.__huntQueue = Queue.Queue(maxsize=maxsize)
+		self.__maxCount = maxsize
+		self.__huntQueue = deque(maxlen=maxsize)
 		self.__recentWeapons = deque(maxlen=weaponCount)
 		self.__recentMonsters = deque(maxlen=monsterCount)
 		self.__currentHunt = None
-		self.__random = random.Random(uuid.uuid1().int)
+		self.__random = random.Random()
 
 	def is_empty(self):
-		return self.__huntQueue.empty()
+		if self.__huntQueue:
+			return False
+		else:
+			return True
 
 	def next_hunt(self):
 		try:
-			self.__currentHunt = self.__huntQueue.get()
+			self.__currentHunt = self.__huntQueue.popleft()
 		except Empty:
 			# Catch Empty exception if attempting .get() on an empty queue.
 			self.__currentHunt = None
@@ -34,7 +37,7 @@ class HuntQueue:
 		return self.__currentHunt
 
 	def is_full(self):
-		return self.__huntQueue.full()
+		return len(self.__huntQueue) == self.__maxCount
 
 	def add_hunt(self, username, user, weapon=None, monster=None):
 		if monster is None:
@@ -43,7 +46,7 @@ class HuntQueue:
 		if weapon is None:
 			weapon = self.roll_weapon()
 
-		self.__huntQueue.put(HuntData(username, user, weapon, monster))
+		self.__huntQueue.append(HuntData(username, user, weapon, monster))
 		self.__recentMonsters.append(monster)
 		self.__recentWeapons.append(weapon)
 
@@ -57,20 +60,18 @@ class HuntQueue:
 		elif len(rollableMonsters) == 1:
 			monster = rollableMonsters[0]
 		else:
-			monster = monster if monster else \
-				self.__random.choice([monster for monster in rollableMonsters if monster not in self.__recentMonsters])
+			monster = self.__random.choice([monster for monster in rollableMonsters if monster not in self.__recentMonsters])
 		return monster
 
 	def roll_weapon(self):
 		rollableWeapons = weapons.get_weapon_names_for_roll()
-		weapon = "Any"
+		weapon = "Any" + str(len(rollableWeapons))
 		if len(rollableWeapons) == 0:
 			return weapon
 		elif len(rollableWeapons) == 1:
 			weapon = rollableWeapons[0]
 		else:
-			weapon = weapon if weapon else \
-				self.__random.choice([weapon for weapon in rollableWeapons if weapon not in self.__recentWeapons])
+			weapon = self.__random.choice([weapon for weapon in rollableWeapons if weapon not in self.__recentWeapons])
 		return weapon				
 
 
